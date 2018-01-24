1f963a0cec15ac04b5d781efd9ec223b2ee62add
Allan Ing <xeero@yahoo.com>
2018-01-23 22:44:07 -0600

    Add Zcash Miner on Nanopool

---
+++ init/start_zm_nanopool.bash
@@ -0,0 +1,7 @@
+# Launches dstm's Zcash Miner directly
+# Or run zm_nanopool_launcher.sh to run in a screen session
+
+# Mine Zcash
+# --pass   Not used
+cd /home/allan/
+./zm_0.5.8/zm --server zec-us-west1.nanopool.org --port 6666 --user ADDRESS.WORKER/EMAIL --pass z
